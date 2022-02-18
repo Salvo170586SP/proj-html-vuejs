3faffbf9c5783a1906030b797cf29f3d592f69bf
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-18 11:14:26 +0100

    added header components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: { Header },
+};
 </script>
 
-<style lang="scss">
 
+
+<style lang="scss">
+@import "./assets/scss/style.scss";
+
+/* da eliminare */
+[class^="col"] {
+  border: 1px solid red;
+}
+.container {
+  border: 1px dotted blue;
+}
+/* da eliminare  */
+
+
+header{
+  background-color: #527CEB;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+      <div class="container text-white">
+        <!-- header-top -->
+         <div id="header-top" class="row">
+           <div class="col-3">
+             img logo
+           </div>
+           <div class="col-9">
+             nav menu
+           </div>
+         </div>
+         <!-- header-bottom/spotlyght -->
+        <div id="header-bottom" class="row">
+          <div class="col-4">descrizione</div>
+          <div class="col-8">immagine</div>
+        </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+
+</style>
