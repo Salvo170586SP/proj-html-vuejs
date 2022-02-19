6bfbd797260c1c2de2ec622cfc6c5a9bba660b6f
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-19 21:39:58 +0100

    added footer bottom

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,24 @@
     </main>
     <footer>
       <FooterTop />
+      <div id="footer-bottom" class="text-white">
+        <div class="container">
+          <div class="row">
+            <div
+              class="col-12 d-flex justify-content-between align-items-center"
+            >
+              <p>@2020 Phlox.All Rights Reserved</p>
+
+              <div class="social">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-linkedin-in"></i>
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-twitter"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
@@ -38,7 +56,7 @@ export default {
     SectionBlog,
     SectionTestimonial,
     SectionSubscribe,
-    FooterTop
+    FooterTop,
   },
   data() {
     return {
@@ -69,5 +87,16 @@ export default {
 }
 /* da eliminare  */
 
+#footer-bottom {
+  height: 80px;
+  background-color: $bg-blue;
+}
 
+p{
+  color: $paragraph-color;
+}
+
+i {
+  margin-left: 20px;
+}
 </style>
