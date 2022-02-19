7f7965f5d7cb1c9ba03724d4a07deecad3c6f359
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-19 16:56:26 +0100

    added all card section blog

--- src/components/SectionBlog.vue
+++ src/components/SectionBlog.vue
@@ -13,22 +13,53 @@
       </div>
       <div class="row">
         <div class="col-6">
-          <div class="card-blog d-flex align-items-end p-3 text-white">
-            <div class="text-card">
-              <span>April 21, 2019 by Paul</span>
+          <!-- card blog -->
+          <div class="card-blog bg-img d-flex align-items-end p-3 text-white">
+            <div class="text-card w-text">
+              <span>April 21, 2019  &#8226;   by Paul</span>
               <h5>Amazon gets CCI nod to aquire 49% stake in</h5>
               <p>
                 When, while the lovely valley teems with vapour around meand the
                 meridian sun strikes the upper surface.
               </p>
             </div>
-            <div class="link-card mb-5">
+            <div class="link-card w-link mb-5">
               <a class="read-btn" href="#">Read More</a>
             </div>
           </div>
         </div>
-        <div class="col-3">item card</div>
-        <div class="col-3">item card</div>
+        <div class="col-3">
+          <!-- card-blog -->
+          <div class="card-blog bg-blue text-white p-5">
+            <div class="text-card mb-4">
+              <span>April 21, 2019  &#8226;  by Paul</span>
+              <h5 class="my-4">Amazon gets CCI nod to aquire 49% stake in</h5>
+              <p class="my-4">
+                When, while the lovely valley teems with vapour around me, the
+                meridian sun s...
+              </p>
+            </div>
+            <div class="link-card">
+              <a class="read-btn" href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <!-- card-blog -->
+          <div class="card-blog bg-blue text-white p-5">
+            <div class="text-card mb-4">
+              <span>April 21, 2019  &#8226;  by Paul</span>
+              <h5 class="my-4">Amazon gets CCI nod to aquire 49% stake in</h5>
+              <p class="my-4">
+                When, while the lovely valley teems with vapour around me, the
+                meridian sun s...
+              </p>
+            </div>
+            <div class="link-card">
+              <a class="read-btn" href="#">Read More</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -52,18 +83,25 @@ export default {
 }
 
 .card-blog {
+  border-radius: 25px;
   height: 340px;
+}
+
+.bg-img {
   background-image: url("../assets/images/headway-537308-unsplash-1380x703.jpg");
   background-position: center;
   background-size: cover;
-  border-radius: 25px;
 }
 
-.text-card {
+.bg-blue {
+  background-color: $bg-blue;
+}
+
+.w-text {
   width: 70%;
 }
 
-.link-card {
+.w-link {
   width: 30%;
 }
 
