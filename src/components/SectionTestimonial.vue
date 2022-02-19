d4e0cf7f686920076c8c6d263dab0a413fad032e
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-19 18:09:34 +0100

    added card testimonial

--- src/components/SectionTestimonial.vue
+++ src/components/SectionTestimonial.vue
@@ -11,6 +11,93 @@
           </p>
         </div>
       </div>
+      <div class="row">
+        <div class="col-4">
+            <!-- card testimonial -->
+          <div class="card-testimonial py-5 d-flex flex-column align-items-center">
+            <div class="img-card">
+              <figure>
+                <img class="img-fluid"
+                  src="../assets/images/szabo-viktor-1266895-unsplash-1024x1024.png"
+                  alt=""
+                />
+              </figure>
+              <div class="vote py-4">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+              </div>
+            </div>
+            <div class="text-card text-center">
+              <p>
+                When, while the lovely valley teems with vapour around meand the
+                meridian sun strikes the upper surface. of the impenetrable
+                foliage of my trees and but a thousand
+              </p>
+              <h6>Michkel Anegl</h6>
+              <p>Marketing Management</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+            <div class="card-testimonial py-5 d-flex flex-column align-items-center">
+            <div class="img-card">
+              <figure>
+                <img class="img-fluid"
+                  src="../assets/images/philipe-cavalcante-539505-unsplash-1024x1024.png"
+                  alt=""
+                />
+              </figure>
+              <div class="vote py-4">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+              </div>
+            </div>
+            <div class="text-card text-center">
+              <p>
+                When, while the lovely valley teems with vapour around meand the
+                meridian sun strikes the upper surface. of the impenetrable
+                foliage of my trees and but a thousand
+              </p>
+              <h6>Michkel Anegl</h6>
+              <p>Marketing Management</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+            <div class="card-testimonial py-5 d-flex flex-column align-items-center">
+            <div class="img-card">
+              <figure>
+                <img class="img-fluid"
+                  src="../assets/images/fabio-spinelli-695744-unsplash-1024x1024.png"
+                  alt=""
+                />
+              </figure>
+              <div class="vote py-4">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+              </div>
+            </div>
+            <div class="text-card text-center">
+              <p>
+                When, while the lovely valley teems with vapour around meand the
+                meridian sun strikes the upper surface. of the impenetrable
+                foliage of my trees and but a thousand
+              </p>
+              <h6>Michkel Anegl</h6>
+              <p>Marketing Management</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -24,7 +111,19 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/scss/style.scss";
 
-.red-span{
-    color: $red-span;
+.red-span {
+  color: $red-span;
 }
+
+figure {
+  width: 100px;
+  border-radius: 50%;
+  overflow: hidden;
+  box-shadow: 0 0 0 5px $brd-color;
+}
+
+.vote{
+    color: $brd-color;
+}
+ 
 </style>
