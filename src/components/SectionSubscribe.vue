180540e962c4da180a8f617dd65cff8d7b4df920
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-19 19:31:54 +0100

    added background card input

--- src/components/SectionSubscribe.vue
+++ src/components/SectionSubscribe.vue
@@ -2,17 +2,23 @@
   <section id="subscribe" class="d-flex align-items-center">
     <div class="container">
       <div class="row">
-        <div class="col-12 d-flex">
-          <div class="text-subscribe">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur blanditiis velit nisi eaque sit iure nemo officiis sed,
-            alias consectetur provident excepturi eos numquam repellendus
-            eveniet repudiandae, veniam quas consequatur?
-          </div>
-          <div class="input-subscribe">
-              <input type="text">
+        <div class="col-12">
+
+            <!-- card-subscribe -->
+          <div class="card-subscribe text-white d-flex align-items-center">
+            <div class="text-subscribe">
+              <h2>We Offer Awesome Services</h2>
+              <p>
+                When, while the lovely valley teems with vapour around meand the
+                meridian sun strikes the upper surface.
+              </p>
+            </div>
+            <div class="input-subscribe">
+              <input type="text" />
               <button>Subscribe</button>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
@@ -33,12 +39,18 @@ export default {
   background-image: linear-gradient(to bottom, white 50%, $bg-blue 50%);
 }
 
-.text-subscribe{
-    width: 50%;
+.card-subscribe{
+    height: 300px;
+    background-color: $red-span;
+    border-radius: 30px;
 }
 
-.input-subscribe{
-    width: 50%;
-    text-align: end;
+.text-subscribe {
+  width: 50%;
+}
+
+.input-subscribe {
+  width: 50%;
+  text-align: end;
 }
 </style>
