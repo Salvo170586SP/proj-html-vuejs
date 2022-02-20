da94592ab86f57ac1827864b85ce55851dc1ae0c
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-20 12:00:39 +0100

    added gradient  section subscribe

--- src/components/SectionSubscribe.vue
+++ src/components/SectionSubscribe.vue
@@ -3,8 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-
-            <!-- card-subscribe -->
+          <!-- card-subscribe -->
           <div class="card-subscribe text-white d-flex align-items-center">
             <div class="text-subscribe px-5">
               <h2>We Offer Awesome Services</h2>
@@ -15,10 +14,9 @@
             </div>
             <div class="input-subscribe">
               <input type="text" placeholder="Enter Your Mail Address" />
-              <button>Subscribe</button>
+              <button class="bg-blue">Subscribe</button>
             </div>
           </div>
-
         </div>
       </div>
     </div>
@@ -39,40 +37,45 @@ export default {
   background-image: linear-gradient(to bottom, $bg-white 50%, $bg-blue 50%);
 }
 
-.card-subscribe{
-    height: 260px;
-    background-color: $red-span;
-    border-radius: 30px;
+.card-subscribe {
+  height: 260px;
+  background: rgb(0,29,36);
+background: linear-gradient(90deg, rgba(0,29,36,1) 0%, rgba(100,133,145,1) 0%, rgba(227,18,47,1) 100%);
+  border-radius: 30px;
 }
 
 .text-subscribe {
   width: 70%;
 }
 
-h2{
-    font-size: 3rem;
-    font-weight: 700;
+h2 {
+  font-size: 3rem;
+  font-weight: 700;
 }
 
 .input-subscribe {
-    width: 365px;
+  width: 365px;
   text-align: end;
   background-color: $bg-white;
   border-radius: 25px;
   padding: 5px;
 }
 
-input{
-    width: 250px;
-    outline: none;
-    border: 0;
+input {
+  width: 250px;
+  outline: none;
+  border: 0;
 }
 
-button{
-    border-radius: 25px;
-    padding: 8px;
-    background-color: $bg-blue;
-    color: $bg-white;
-    border: none;
+button {
+  border-radius: 25px;
+  padding: 8px;
+  border: none;
+}
+
+button.bg-blue {
+background: rgb(2,0,36);
+background: linear-gradient(270deg, rgba(2,0,36,1) 0%, rgba(36,36,115,1) 0%, rgba(0,160,186,1) 100%);
+  color: $bg-white;
 }
 </style>
