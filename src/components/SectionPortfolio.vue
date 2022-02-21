b8c70971331bd153737362a60a2e48e55a2645a1
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-21 12:06:40 +0100

    added hover card portfolio

--- src/components/SectionPortfolio.vue
+++ src/components/SectionPortfolio.vue
@@ -6,52 +6,65 @@
           <span>Portfolio</span>
           <h2><strong>lastest</strong> work</h2>
         </div>
-        <div class="col-6"></div>
+        <div class="col-6 d-flex align-items-center justify-content-end">
+          <i class="fa-solid fa-arrow-left-long"></i>
+          <i class="fa-solid fa-arrow-right-long"></i>
+        </div>
       </div>
 
-      <!-- card portfolio -->
+      <!-- cards portfolio -->
       <div class="row">
         <div class="col-4">
-          <figure>
-            <img
-              class="img-fluid"
-              src="../assets/images/DRY-1-790x576.jpg"
-              alt=""
-            />
-          </figure>
-          <div class="row">
-            <div class="col-6 fw-bold">Purinky Products</div>
-            <div class="col-6">Digital Experience</div>
+          
+          <!-- card -->
+          <div class="card bg-red">
+            <figure>
+              <img
+                class="img-fluid"
+                src="../assets/images/8wa60okr-1-790x576.jpg"
+                alt=""
+              />
+            </figure>
+            <div class="row py-2">
+              <div class="col-7 fw-bold">Bascket of flower on table</div>
+              <div class="col-5">Branding Strategy</div>
+            </div>
           </div>
         </div>
 
         <div class="col-4">
-          <figure>
-            <img
-              class="img-fluid"
-              src="../assets/images/8wa60okr-1-790x576.jpg"
-              alt=""
-            />
-          </figure>
-          <div class="row">
-            <div class="col-6 fw-bold">Basket of flower on table</div>
-            <div class="col-6">Branding strategy</div>
+          <!-- card -->
+          <div class="card bg-red">
+            <figure>
+              <img
+                class="img-fluid"
+                src="../assets/images/DRY-1-790x576.jpg"
+                alt=""
+              />
+            </figure>
+            <div class="row py-2">
+              <div class="col-7 fw-bold">Purinky Products</div>
+              <div class="col-5">Digital Experience</div>
+            </div>
           </div>
         </div>
 
         <div class="col-4">
-          <figure>
-            <img
-              class="img-fluid"
-              src="../assets/images/84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg"
-              alt=""
-            />
-          </figure>
-          <div class="row">
-            <div class="col-6 fw-bold">Basket of flower on table</div>
-            <div class="col-6">Branding strategy</div>
+          <!-- card -->
+          <div class="card bg-red">
+            <figure>
+              <img
+                class="img-fluid"
+                src="../assets/images/a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg"
+                alt=""
+              />
+            </figure>
+            <div class="row py-2">
+              <div class="col-7 fw-bold">Satisfy Poster</div>
+              <div class="col-5">Branding Strategy</div>
+            </div>
           </div>
-        </div>       
+        </div>
       </div>
 
       <div class="row">
@@ -78,9 +91,35 @@ span {
   color: $red-span;
 }
 
-figure {
+i {
+  color: $red-span;
+  cursor: pointer;
+  border: 1px solid $red-span;
+  margin-right: 15px;
+  border-radius: 50%;
+  padding: 15px;
+}
+
+.card {
   border-radius: 20px;
+  text-align: center;
+  cursor: pointer;
+  border: 0;
+
+  &:hover.bg-red {
+    background: linear-gradient(
+      90deg,
+      rgba(0, 29, 36, 1) 0%,
+      rgba(100, 133, 145, 1) 0%,
+      rgba(227, 18, 47, 1) 100%
+    );
+    color: white;
+  }
+}
+
+figure {
   overflow: hidden;
+  border-radius: 20px;
 }
 
 .dot-img {
